3ceb0215db5c900be4223697d2a0a7acbae94d10
OlesiaVysotskaia <olesiavysorskaia@gmail.com>
2020-02-14 12:49:05 +0300

    1 приммер

---
+++ 06_anime/01-basics/javascript/main.js
@@ -0,0 +1,15 @@
+let targetsExample01 = anime({
+  targets: '.box',
+  translateX: 250,
+  autoplay; false
+})
+
+let targetsPlayButton =
+document.queryselector('.targets button')
+
+targetsPlayButton.onclick = targetsExample01.play
+
+anime({
+  targets: '.triangle',
+  translateX; 150
+})
